ba61fa6e2bb1b4001a222cf63e1f1299ba714950
haoranliu <2251561318@qq.com>
2019-06-11 09:18:07 +0800

    1

--- hard/Longest Duplicate Substring.js
+++ hard/Longest Duplicate Substring.js
@@ -24,22 +24,56 @@
 // };
 
 var longestDupSubstring = function(S) {
-  let mid = parseInt(S.length / 2);
+  let R = 26,
+    MOD = 1e9 + 7;
+
   let left = 0,
-    right = S.length;
+    right = S.length - 1;
 
   while (left < right) {
-    if (hasSubstr(S, mid) != '') left = mid;
+    let mid = Math.ceil((left + right) / 2);
+    if (rabinKarp(S, mid) != '') left = mid;
     else right = mid - 1;
-    mid = parseInt((left + right + 1) / 2);
   }
 
-  return hasSubstr(S, left);
+  return rabinKarp(S, left);
 
   function hasSubstr(S, num) {
     for (let i = 0; i < S.length - num; i++) {
       let subS = S.substr(i, num);
       if (S.lastIndexOf(subS) > i) return subS;
+    }
+    return '';
+  }
+
+  function rabinKarp(str, len) {
+    let RM = 1;
+    // 等价于RM=Math.pow(R,M-1) % MOD
+    // 由于JS精度问题拆解计算
+    for (let i = 1; i < len; i++) {
+      RM = (RM * R) % MOD;
+    }
+    let map = new Map();
+    let num = 0;
+    // 计算前len个字符串的散列值
+    for (let i = 0; i < len; i++) {
+      let code = str.charCodeAt(i) - 97;
+      num = (num * R + code) % MOD;
+    }
+    map.set(num, 0);
+    // 后续计算散列值
+    for (let i = 0; i < str.length - len; i++) {
+      let preCode = str.charCodeAt(i) - 97,
+        curCode = str.charCodeAt(i + len) - 97;
+      num = (num + MOD - ((preCode * RM) % MOD)) % MOD;
+      num = (num * R + curCode) % MOD;
+      if (map.has(num)) {
+        let sub = str.substring(i + 1, i + 1 + len);
+        let preId = map.get(num),
+          preSub = str.substring(preId, preId + len);
+        if (sub === preSub) return sub;
+      }
+      map.set(num, i + 1);
     }
     return '';
   }
