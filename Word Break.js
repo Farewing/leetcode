afda21f1b6901da6647dea121ff3ad019035097e
Farewing <2251561318@qq.com>
2019-06-12 23:23:33 +0800

    l

--- Word Break.js
+++ Word Break.js
@@ -3,20 +3,41 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function (s, wordDict) {
-  let dp = [];
+var wordBreak = function(s, wordDict) {
+  let dp = [],
+    m = [];
   for (let i = 0; i <= s.length; i++) {
-    dp[i] = false;
+    dp[i] = {
+      result: false,
+      parent: []
+    };
+    m[i] = [];
   }
-  dp[0] = true;
+  dp[0] = { result: true };
 
   for (let i = 0; i <= s.length; i++) {
     for (let j = 0; j < i; j++) {
-      if (dp[j] && wordDict.includes(s.substr(j, i - j))) {
-        dp[i] = true;
-        break;
+      if (dp[j].result && wordDict.includes(s.substr(j, i - j))) {
+        dp[i].result = true;
+        dp[i].parent.push(j);
+        // break;
       }
     }
   }
-  return dp[s.length];
+
+  if (!dp[s.length].result) return [];
+
+  for (let i = 1; i <= s.length; i++) {
+    if (!dp[i].result) continue;
+    for (let ele of dp[i].parent) {
+      if (m[ele].length == 0) m[i].push(s.substr(ele, i - ele));
+      else {
+        for (let e of m[ele]) {
+          m[i].push(e + ' ' + s.substr(ele, i - ele) + '');
+        }
+      }
+    }
+  }
+  console.log(m);
+  return m[s.length];
 };
